5e4e6769d0abaa06fa659d84d6a0402b14afd3bf
sanotogii <nasaereh@gmail.com>
2024-05-25 07:59:11 +0200

    adding stats route - task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,34 @@
 """Index view for API status"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    amenities = storage.count(Amenity)
+    cities = storage.count(City)
+    places = storage.count(Place)
+    reviews = storage.count(Review)
+    states = storage.count(State)
+    users = storage.count(User)
+    return jsonify({
+        "amenities": amenities,
+        "cities": cities,
+        "places": places,
+        "reviews": reviews,
+        "states": states,
+        "users": users,
+    })
