44c25cc0ce2e535cebd1fa2e55cc1497a3a950f6
sanotogii <nasaereh@gmail.com>
2024-05-27 08:48:34 +0200

    CORS task-12

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -24,6 +25,7 @@ def page_not_found(error):
 
 
 if __name__ == "__main__":
+    CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
