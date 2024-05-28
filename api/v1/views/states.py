0486b0b2290a89b3a6a7d3b4bb77e3af979cce70
Ebube-Ochemba <ebube17@gmail.com>
2024-05-28 22:43:38 +0100

    refactor

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -20,20 +20,22 @@ def get_states():
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """Retrieves a State object"""
-    state_obj = storage.get(State, state_id)
-    if state_obj is None:
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
-    return jsonify(state_obj.to_dict())
+
+    return jsonify(state.to_dict())
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_state(state_id):
     """Deletes a State object"""
-    state_obj = storage.get(State, state_id)
-    if state_obj is None:
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
-    storage.delete(state_obj)
+
+    storage.delete(state)
     storage.save()
     return jsonify({}), 200
 
@@ -41,15 +43,14 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """Creates a state"""
-    try:
-        data = request.get_json()
-    except Exception:
+    req_json = request.get_json()
+    if not req_json:
         abort(400, description="Not a JSON")
 
-    if 'name' not in data:
+    if 'name' not in req_json:
         abort(400, description="Missing name")
 
-    new_state = State(**data)
+    new_state = State(**req_json)
     new_state.save()
     return jsonify(new_state.to_dict()), 201
 
@@ -60,16 +61,16 @@ def update_state(state_id):
     if request.content_type != "application/json":
         abort(400, description="Not a JSON")
 
-    data = request.get_json()
-    if not data:
+    req_json = request.get_json()
+    if not req_json:
         abort(400, description="Not a JSON")
 
-    state_obj = storage.get(State, state_id)
-    if state_obj is None:
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
 
-    for key, value in data.items():
+    for key, value in req_json.items():
         if key not in ['id', 'created_at', 'updated_at']:
-            setattr(state_obj, key, value)
-    state_obj.save()
-    return jsonify(state_obj.to_dict()), 200
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict()), 200
